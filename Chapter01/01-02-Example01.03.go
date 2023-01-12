dff7aafc8c2fcb4bac15f29ee812c25f850b854d
Elliot <elliotcs30@gmail.com>
2023-01-12 13:18:54 +0800

    feat: add var type issue exercise

---
+++ Chapter01/01-02-Example01.03.go
@@ -0,0 +1,16 @@
+package main
+
+import "math/rand"
+
+func main() {
+	// var seed = 1234456789
+	
+	// 宣告得到的變數會出現問題
+	// 因為 seed 需要的參數型別為 int64
+	// rand.Seed(seed)
+
+	// cannot use seed (variable of type int) as type int64 in argument to rand.Seed
+
+	var seed int64 = 1234456789
+	rand.Seed(seed)
+}
