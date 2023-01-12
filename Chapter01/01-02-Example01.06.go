cbb8d9ccd6ebfcb8ef66349701b835594a5fcec3
Elliot <elliotcs30@gmail.com>
2023-01-12 14:03:14 +0800

    feat: var not english declaration(more)

---
+++ Chapter01/01-02-Example01.06.go
@@ -0,0 +1,16 @@
+// 非英文的變數名稱宣告
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	デバッグ := false
+	日誌等級 := "info"
+	ይጀምሩ := time.Now()
+	_A1_Μείγμα := ""
+	fmt.Println(デバッグ, 日誌等級, ይጀምሩ, _A1_Μείγμα)
+}
